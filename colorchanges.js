661056dfcedcc63d0e57503e8be47dc310a916aa
sumit <mauryasumit222@gmail.com>
2024-01-08 16:45:17 +0530

    PD DESIGN

---
+++ colorchanges.js
@@ -0,0 +1,122 @@
+// // Banner Section 1
+
+// function myfun3() {
+//     fontjs.style.fontSize = "25px";
+//     fontjs.style.transition = "0.4s";
+
+// }
+// function myfun4() {
+//     fontjs.style.fontSize = "";
+// }
+
+
+// function myfun1() {
+//     bannersection1.style.backgroundColor = "black";
+    
+// }
+// function myfun2() {
+//     bannersection1.style.background = "";
+//     bannersection1.style.color = "black";
+    
+// }
+
+// var my1fun = document.getElementById("bannersection1");
+// my1fun.addEventListener('mouseover', myfun1);
+// my1fun.addEventListener('mouseleave', myfun2);
+// my1fun.addEventListener('mouseover',myfun3)
+// my1fun.addEventListener('mouseleave',myfun4)
+
+
+
+// // Banner Section 2
+
+// function banner2fun1() {
+//     bannersection2.style.backgroundColor = "black";
+//     bannersection2.style.Color = "white";
+
+// }
+// function banner2fun2() {
+//     bannersection2.style.backgroundColor = "";
+// }
+
+// function banner2funpara() {
+//     para.style.color = "white";
+// }
+
+// var banner2fun = document.getElementById("bannersection2");
+// banner2fun.addEventListener('mouseover', banner2fun1);
+// banner2fun.addEventListener('mouseover', banner2funpara);
+// banner2fun.addEventListener('mouseleave', banner2fun2);
+
+// // Banner section 3
+
+// function bannersection3fun() {
+//     bannersection3img.style.width = "400px";
+// }
+// function bannersection3imggg() {
+//     bannersection3img.style.width = "";
+// }
+
+// var banner3fun = document.getElementById("bannersection3img");
+// banner3fun.addEventListener('mouseover',bannersection3fun)
+// // Our service section
+
+
+// // const ourservicesectionbtn = document.querySelector("ourservicesectionbtn");
+
+// ourservicesectionbtn.addEventListener('click', () => {
+//     ourservicesectionbtn.classList.add('mouseover');
+//     ourservicesectionbtn.style.borderBlockEnd = "red";
+// })
+
+
+
+
+
+
+// // footer section
+
+// // var footerline = document.getElementById("footerline");
+
+// // var footerValue =  footerline.style.transition
+
+
+// // footerline.onmouseover = function () {
+// //     footerline.style.transition
+// //     footerline.style.borderBottomColor = "#fff";
+// //     footerline.style.width = "2px solid red"
+// // }
+
+
+// // const footerline = document.getElementById("footerline");
+
+
+
+
+// // Nav bar section
+// // Swipper navbar
+
+// window.onscroll = () => {
+//     let header = document.querySelector('.header');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+// }
+
+
+
+
+
+// // Crolling BTN
+
+// var scrollBtn = document.getElementById("scrollBtn");
+
+// scrollBtn.addEventListener('click', () => window.scroll({
+//     top: 0,
+//     behavior: 'smooth',
+// }));
+
+
+
+
+
+
+
